968e74c42bdaec8a5b8c416fa26b56995d23c0a4
Fowo <fowoadeniji2@gmail.com>
2025-09-30 10:37:08 +0100

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
-import CurrentWeather from './components/CurrentWeather';
 import Weather from './pages/Weather';
 
 
